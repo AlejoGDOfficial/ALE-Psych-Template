f6f09039a0126e0251d6b71544d3f2a155c22811
AlejoGDOfficial <alejogdoff@gmail.com>
2025-06-19 18:33:33 -0500

    Epic Video Support

--- setup/install-haxelibs.bat
+++ setup/install-haxelibs.bat
@@ -6,6 +6,7 @@ echo Installing dependencies.
 @if not exist ".haxelib\" mkdir .haxelib
 
 haxelib install lime 8.2.2
+haxelib run lime setup
 haxelib install openfl 9.4.1
 haxelib install flixel 5.6.1
 haxelib install flixel-addons 3.2.3
@@ -16,7 +17,7 @@ haxelib install hxcpp 4.3.2
 haxelib install hxdiscord_rpc 1.2.4 --skip-dependencies
 haxelib git flxanimate https://github.com/ShadowMario/flxanimate dev
 haxelib git linc_luajit https://github.com/superpowers04/linc_luajit 633fcc051399afed6781dd60cbf30ed8c3fe2c5a
-haxelib install hxvlc 2.2.1
+haxelib install hxvlc 2.2.2 --skip-dependencies
 
 echo Finished!
 pause
